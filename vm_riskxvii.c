552f93b693e83f17a63c8d0fce9bd1ff054ef5ae
Max Dalgleish <maxdalgleish@gmail.com>
2023-03-18 00:58:21 +1100

    updating

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -12,15 +12,11 @@ int main(int argc, char **argv) {
     int registers[32] = {0};
     int pc_val = 0;
     FILE *bin_input = fopen(argv[1], "rb");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 10; i++) {
         int a = fgetc(bin_input);
         int b = fgetc(bin_input);
         int c = fgetc(bin_input);
         int d = fgetc(bin_input);
-        printf("%x ", a);
-        printf("%x ", b);
-        printf("%x ", c);
-        printf("%x ", d);
 
         int instruction = ((((((d << 8) | c) << 8) | b) << 8) | a);
         printf("%x ", instruction);
@@ -29,17 +25,20 @@ int main(int argc, char **argv) {
         int opcode = instruction & 0x7F;
         switch (opcode)
         {
+            // addi
             case 0b0010011: {
-                int func3 = instruction & (0b111 << 12);
-                if (func3 == 0b000) {
-                    int rd = (instruction >> 7) & 0b11111;
-                    int rs1 = (instruction >> 15) & 0b11111;
+                int func3 = (instruction >> 12) & 0b111;
+                if (func3 == 0b0) {
+                    int rd = (instruction >> 7) & 0x1F;
+                    int rs1 = (instruction >> 15) & 0x1F;
                     int imm = (instruction >> 20) & 0xFFF;
+                    // printf("%d %d %d\n", rd, rs1, imm);
                     registers[rd] = registers[rs1] + imm;
-                    printf("%d\n", registers[rd]);
                 }
+                pc_val += 4;
                 break;
             }
+            // jal
             case 0b1101111: {
                 int rd = (instruction >> 7) & 0b11111;
                 registers[rd] = pc_val + 4;
@@ -48,14 +47,28 @@ int main(int argc, char **argv) {
                 int imm3 = (instruction >> 12) & 0xFF;
                 int imm4 = (instruction >> 31) & 0b1;
                 int imm = ((((((imm4 << 8) | imm3) << 1) | imm2) << 10) | imm1);
-                printf("%d\n ", imm);
-
+                pc_val = pc_val + (imm << 1);
+                fseek(bin_input, pc_val, SEEK_SET);
                 break;
+            }
+            // lui
+            case 0b0110111: {
+                int rd = (instruction >> 7) & 0x1F;
+                int imm = ((instruction >> 12) & 0xFFFFF) << 12;
+                registers[rd] = imm;
+                break;
+            }
+            
+            case 0b0100011: {
+                int func3 = (instruction >> 12) & 0b111;
+                // Sb
+                if (func3 == 0b0) {
+                    
+                }
             }
             default:
                 break;
         }
-        pc_val += 4;
     }
     return 0;
 }
