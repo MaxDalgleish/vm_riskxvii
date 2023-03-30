3ccf69d3f3dc070071b8cdaf0c1b3c95db972d47
Max Dalgleish <maxdalgleish@gmail.com>
2023-03-30 21:59:53 +1100

    testing heap banks

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -513,13 +513,13 @@ int main(int argc, char **argv) {
 					}
 				}
 				int value = 0;
-				value = heap_banks[0].data[0];
+				value = value | heap_banks[0].data[0];
 				value = value << 8;
-				value = heap_banks[0].data[1];
+				value = value | heap_banks[0].data[1];
 				value = value << 8;
-				value = heap_banks[0].data[2];
+				value = value | heap_banks[0].data[2];
 				value = value << 8;
-				value = heap_banks[0].data[3];
+				value = value | heap_banks[0].data[3];
 				printf("value is: %d", value);
 				break;
 			}
